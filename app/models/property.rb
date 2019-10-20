b0f5523b55b9ab19a579e0d50e7779c69dabb676
Muneeb <muneeb@ginzametrics.com>
2019-10-21 04:17:38 +0500

    detailed crawling is added

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,6 +4,7 @@ require 'open-uri'
 
 class Property < ApplicationRecord
   include Sidekiq::Worker
+
   def perform(pd_id)
     pq = PropertyQueue.find(pd_id)
     return unless pq.status == 'pending'
@@ -11,27 +12,37 @@ class Property < ApplicationRecord
     location = Location.find(pq.location_id)
     unless location.locationidentifier.nil?
       # We can scrap the rightmove.co.uk for this keyword
-      url = "https://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=#{location.locationidentifier}&insId=1&radius=0.0"
-      doc = Nokogiri::HTML(open(url.strip))
       properties_data = []
+      need_to_break = false
+      for i in 0 .. 45
+        index = i * 24
+        begin
+          url = "https://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=#{location.locationidentifier}&index=#{index}&insId=1&radius=0.0"
+          doc = Nokogiri::HTML(open(url.strip))
+          old_properties_data_count = properties_data.count
+          doc.css('div.l-searchResult.is-list.is-not-grid').each do |property|
+            property_images = property.css('img').map{|p| p['src'] if p['src'].present?}.compact
+            property_price = property.css('div.propertyCard-priceValue').first.text.strip.gsub(',','')[1..-1].to_f rescue 0.0
+            source_url = (property.css('.propertyCard-link').first['href']) rescue nil
+            next unless source_url.present?
+            source_url = 'https://www.rightmove.co.uk' + source_url
+            properties_data << {
+                images: property_images,
+                name: property.css('h2').first.text.strip,
+                description: property.css('.propertyCard-link').text.strip.tr("\n",""),
+                price: property_price,
+                source: 'Rightmove',
+                source_url: source_url,
+                created_at: Time.now,
+                updated_at: Time.now
 
-      doc.css('div.l-searchResult.is-list.is-not-grid').each do |property|
-        property_images = property.css('img').map{|p| p['src'] if p['src'].present?}.compact
-        property_price = property.css('div.propertyCard-priceValue').first.text.strip.gsub(',','')[1..-1].to_f rescue 0.0
-        source_url = (property.css('.propertyCard-link').first['href']) rescue nil
-        next unless source_url.present?
-        source_url = 'https://www.rightmove.co.uk' + source_url
-        properties_data << {
-            images: property_images,
-            name: property.css('h2').first.text.strip,
-            description: property.css('.propertyCard-link').text.strip.tr("\n",""),
-            price: property_price,
-            source: 'Rightmove',
-            source_url: source_url,
-            created_at: Time.now,
-            updated_at: Time.now
-
-        }
+            }
+          end
+        rescue
+          need_to_break = true
+        end
+        need_to_break = true  if properties_data.count == old_properties_data_count
+        break if need_to_break
       end
 
       Property.insert_all(properties_data)
